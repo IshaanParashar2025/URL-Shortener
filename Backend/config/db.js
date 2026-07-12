cec21786f605846853e8d65a55a097881b910c00
ishaan <ishaan22parashar@gmail.com>
2026-07-12 23:42:41 +0530

    fixed query bug in accessing stats

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,17 +1,6 @@
 const mysql2 = require("mysql2/promise")
 
-const db = mysql2.createPool(
-    {
-<<<<<<< HEAD
-        host:"localhost",
-        user:"root",
-        password:"$trongPassword123",
-        database:"url_db",
-        ssl: {
-            rejectUnauthorized: false
-        }
-
-=======
+const db = mysql2.createPool({       
         host: process.env.mysql_host,
         user: process.env.mysql_user,
         password: process.env.mysql_password,
@@ -20,8 +9,6 @@ const db = mysql2.createPool(
         ssl: {
             rejectUnauthorized: false
         }
->>>>>>> changes
-    }
-)
+});
 
 module.exports = db;
