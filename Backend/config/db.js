53fc109ae2c5f8dc05928fb8c30ab4c0c393f99f
ishaan <ishaan22parashar@gmail.com>
2026-07-12 22:53:38 +0530

    fixed connection problems

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -2,10 +2,14 @@ const mysql2 = require("mysql2/promise")
 
 const db = mysql2.createPool(
     {
-        host:"localhost",
-        user:"root",
-        password:"$trongPassword123",
-        database:"url_db"
+        host: process.env.mysql_host,
+        user: process.env.mysql_user,
+        password: process.env.mysql_password,
+        database: process.env.mysql_database,
+        port: process.env.mysql_port,
+        ssl: {
+            rejectUnauthorized: false
+        }
     }
 )
 
