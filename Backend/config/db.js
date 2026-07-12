893f6d08034f552c7853a0a86a3b90f037914c9e
IshaanParashar2025 <ishaan22parashar@gmail.com>
2026-07-12 23:11:58 +0530

    Update db.js

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -2,16 +2,6 @@ const mysql2 = require("mysql2/promise")
 
 const db = mysql2.createPool(
     {
-<<<<<<< HEAD
-        host:"localhost",
-        user:"root",
-        password:"$trongPassword123",
-        database:"url_db",
-        ssl: {
-            rejectUnauthorized: false
-        }
-
-=======
         host: process.env.mysql_host,
         user: process.env.mysql_user,
         password: process.env.mysql_password,
@@ -20,7 +10,6 @@ const db = mysql2.createPool(
         ssl: {
             rejectUnauthorized: false
         }
->>>>>>> changes
     }
 )
 
